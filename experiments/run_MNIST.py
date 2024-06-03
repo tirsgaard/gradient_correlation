06fe6c5dac5ca1209090c5ee18d32ce4fcbdd326
Rasmus Tirsgaard <rasmustirsgaard@gmail.com>
2024-06-03 14:37:35 +0200

    Refactor MNIST code

--- experiments/run_MNIST.py
+++ experiments/run_MNIST.py
@@ -115,6 +115,8 @@ def run_single_experiment(sampled_indexes: torch.Tensor, unsampled_indexes: torc
     most_decor_points, cov = rank_uncertainty_information(non_train_loader, best_model, loss_batched, optimizer, unc, pre_condition_index=torch.tensor([], dtype=int), cutoff_number=100)
     gradient_covariance_unique = reduced_unsample_indexes[most_decor_points]
     
+    rankings = [passive_sampling_ranking, uncertainty_sampling_ranking, gradient_correlation_ranking, gradient_correlation_cond_ranking, gradient_covariance_unique]
+    
     # Reset the model
     model = Parallel_MLP(input_size=784, output_size=10, hidden_size=config.model.hidden_size, num_layers=config.model.num_layers, num_parallel=config.model.num_parallel).to(device)
     
@@ -132,99 +134,36 @@ def run_single_experiment(sampled_indexes: torch.Tensor, unsampled_indexes: torc
         results_epoch.append(best_epoch)
         return list(zip(*results_epoch))
         
-    random_sampling_results = []
-    uncertainty_sampling_results = []
-    gradient_correlation_results = []
-    gradient_correlation_cond_results = []
-    gradient_covariance_unique_results = []
+    results = []
     for n_samples in n_additional_samples:
-        # Random sampling
-        random_sampling_results.append(run_sampling_experiment(passive_sampling_ranking, n_samples, deepcopy(model), loss))
-        
-        # Uncertainty sampling
-        uncertainty_sampling_results.append(run_sampling_experiment(uncertainty_sampling_ranking, n_samples, deepcopy(model), loss))
-            
-        # Gradient correlation sampling
-        gradient_correlation_results.append(run_sampling_experiment(gradient_correlation_ranking, n_samples, deepcopy(model), loss))
-        
-        # Gradient correlation sampling with conditioning on training data
-        gradient_correlation_cond_results.append(run_sampling_experiment(gradient_correlation_cond_ranking, n_samples, deepcopy(model), loss))
-        
-        # Gradient correlation sampling with correlation to training data gradient
-        gradient_covariance_unique_results.append(run_sampling_experiment(gradient_covariance_unique, n_samples, deepcopy(model), loss))
-        
-    return random_sampling_results, uncertainty_sampling_results, gradient_correlation_results, gradient_correlation_cond_results, gradient_covariance_unique_results
-
-def run_sequence_experiment(indexes: torch.Tensor, n_samples: int, unc_sample: bool=False, corr_sample: bool=False, N_max: int = 100) -> tuple[list[float], list[float]]:
-    """ Run an active learning experiment. 
-    WARNING that the indexes tensor is modified in place.
-    Args:
-        indexes: the indexes to use
-        n_samples: the number of samples to use
-        unc_sample: whether to use uncertainty sampling
-        corr_sample: whether to use correlation sampling
-        N_max: the maximum number of samples to use for correlation sampling
-        
-    Returns:
-        validation_losses: the validation losses
-        validation_accuracies: the validation accuracies
-    """
-    torch.manual_seed(1)
-    model = SimpleMLP(input_size=784, output_size=10, hidden_size=config.model.hidden_size, num_layers=config.model.num_layers)
-    optimizer = torch.optim.Adam(model.parameters(), lr=config.training.learning_rate)
-    
-    indexed_data = torch.utils.data.Subset(train_data, indexes[:n_samples])
-    max_samples = min(len(indexes), n_samples + N_max)
-    non_indexed_data = torch.utils.data.Subset(train_data, indexes[:max_samples] if corr_sample else indexes[n_samples:max_samples])
-    train_loader = torch.utils.data.DataLoader(indexed_data, batch_size=config.training.batch_size, shuffle=True, pin_memory=False)
-    if len(non_indexed_data) > 0:
-        non_train_loader = torch.utils.data.DataLoader(non_indexed_data, batch_size=config.training.batch_size, shuffle=False, pin_memory=False)
-    else:
-        non_train_loader = []
-
-    # Train the model
-    validation_losses = []
-    validation_accuracies = []
-
-    for epoch in range(config.training.num_epochs):
-        # Validate model
-        val_loss, val_accuracy = validate(model, loss, val_loader, device)
-        validation_losses.append(val_loss)
-        validation_accuracies.append(val_accuracy)
-        train_epoch(model, optimizer, loss, train_loader, device)
-    if unc_sample and len(non_train_loader) > 0:
-        # Compute predictions of the non-indexed data
-        most_unc_points = uncertainty_rank_datapoints(non_train_loader, model)
-        indexes[n_samples:max_samples] = indexes[n_samples:max_samples][most_unc_points]
-        
-    if corr_sample and len(non_train_loader) > 0:
-        most_unc_points = rank_sample_information(non_train_loader, model, loss_batched, optimizer, pre_condition_index=torch.tensor(range(n_samples)))
-        indexes[:max_samples] = indexes[:max_samples][most_unc_points]
-        
-    val_loss, val_accuracy = validate(model, loss, val_loader, device)
-    validation_losses.append(val_loss)
-    validation_accuracies.append(val_accuracy)
-        
-    return validation_losses, validation_accuracies
+        results.append([run_sampling_experiment(data_rankings, n_samples, deepcopy(model), loss) for data_rankings in rankings])
+    return results
 
 n_start_data = 1000
-n_rep = 10
-results = []
-for i in tqdm(range(n_rep), desc='Running Repetitions'):
-    indexes = torch.randperm(len(train_data))
-    results.append(run_single_experiment(indexes[:n_start_data], indexes[n_start_data:], n_samples, max_add_subset=10**3))
-    
-result_array = torch.tensor(results)
-sample_validation_losses = result_array[:, 0, :, 0, :]
-sample_validation_accuracy = result_array[:, 0, :, 1, :]
-sample_validation_unc_losses = result_array[:, 1, :, 0, :]
-sample_validation_unc_accuracy = result_array[:, 1, :, 1, :]
-sample_validation_corr_losses = result_array[:, 2, :, 0, :]
-sample_validation_corr_accuracy = result_array[:, 2, :, 1, :]
-sample_validation_corr_cond_losses = result_array[:, 3, :, 0, :]
-sample_validation_corr_cond_accuracy = result_array[:, 3, :, 1, :]
-sample_validation_cov_losses = result_array[:, 4, :, 0, :]
-sample_validation_cov_accuracy = result_array[:, 4, :, 1, :]
+n_rep = 2
+run_experiments = True
+
+if run_experiments:
+    results = []
+    for i in tqdm(range(n_rep), desc='Running Repetitions'):
+        indexes = torch.randperm(len(train_data))
+        results.append(run_single_experiment(indexes[:n_start_data], indexes[n_start_data:], n_samples, max_add_subset=10**3))
+        
+    result_array = torch.tensor(results)  # (n_rep, n_samples, n_sampling_strategies, 2, 2)
+    torch.save(result_array, 'MNIST_results.pt')
+else:
+    result_array = torch.load('MNIST_results.pt')
+
+sample_validation_losses = result_array[:, :, 0,  0, :]
+sample_validation_accuracy = result_array[:, :, 0, 1, :]
+sample_validation_unc_losses = result_array[:, :, 1, 0, :]
+sample_validation_unc_accuracy = result_array[:, :, 1, 1, :]
+sample_validation_corr_losses = result_array[:, :, 2, 0, :]
+sample_validation_corr_accuracy = result_array[:, :, 2, 1, :]
+sample_validation_corr_cond_losses = result_array[:, :, 3, 0, :]
+sample_validation_corr_cond_accuracy = result_array[:, :, 3, 1, :]
+sample_validation_cov_losses = result_array[:, :, 4, 0, :]
+sample_validation_cov_accuracy = result_array[:, :, 4, 1, :]
 
 # Plot the validation loss and accuracy as a function of the number of samples
 fig, axs = plt.subplots(2)
@@ -254,22 +193,22 @@ def generate_uncertainty_curve(ax, data_sample, label, color, significance_level
     ax.fill_between(n_samples, mean - lines, mean + lines, color=color, alpha=0.2)
 
 fig, axs = plt.subplots(2)
-generate_uncertainty_curve(axs[0], sample_validation_losses[..., -1], 'Final Loss PL', 'red')
-generate_uncertainty_curve(axs[0], sample_validation_unc_losses[..., -1], 'Final Loss unc', 'green')
-generate_uncertainty_curve(axs[0], sample_validation_corr_losses[..., -1], 'Final Loss corr', 'blue')
-generate_uncertainty_curve(axs[0], sample_validation_corr_cond_losses[..., -1], 'Final Loss corr cond', 'purple')
-generate_uncertainty_curve(axs[0], sample_validation_cov_losses[..., -1], 'Final Loss cov', 'orange')
+generate_uncertainty_curve(axs[0], sample_validation_losses[..., -1], 'PL', 'red')
+generate_uncertainty_curve(axs[0], sample_validation_unc_losses[..., -1], 'unc', 'green')
+generate_uncertainty_curve(axs[0], sample_validation_corr_losses[..., -1], 'corr', 'blue')
+generate_uncertainty_curve(axs[0], sample_validation_corr_cond_losses[..., -1], 'corr cond', 'purple')
+generate_uncertainty_curve(axs[0], sample_validation_cov_losses[..., -1], 'cov', 'orange')
 axs[0].set_title('Final Loss')
 axs[0].set_xlabel('Number of Samples')
 axs[0].set_ylabel('Loss')
 axs[0].set_xscale('log')
 axs[0].legend()
 
-generate_uncertainty_curve(axs[1], sample_validation_accuracy[..., -1], 'Final Accuracy PL', 'red')
-generate_uncertainty_curve(axs[1], sample_validation_unc_accuracy[..., -1], 'Final Accuracy unc', 'green')
-generate_uncertainty_curve(axs[1], sample_validation_corr_accuracy[..., -1], 'Final Accuracy corr', 'blue')
-generate_uncertainty_curve(axs[1], sample_validation_corr_cond_accuracy[..., -1], 'Final Accuracy corr cond', 'purple')
-generate_uncertainty_curve(axs[1], sample_validation_cov_accuracy[..., -1], 'Final Accuracy cov', 'orange')
+generate_uncertainty_curve(axs[1], sample_validation_accuracy[..., -1], 'Accuracy PL', 'red')
+generate_uncertainty_curve(axs[1], sample_validation_unc_accuracy[..., -1], 'Accuracy unc', 'green')
+generate_uncertainty_curve(axs[1], sample_validation_corr_accuracy[..., -1], 'Accuracy corr', 'blue')
+generate_uncertainty_curve(axs[1], sample_validation_corr_cond_accuracy[..., -1], 'Accuracy corr cond', 'purple')
+generate_uncertainty_curve(axs[1], sample_validation_cov_accuracy[..., -1], 'Accuracy cov', 'orange')
 
 axs[1].set_title('Final Accuracy')
 axs[1].set_xlabel('Number of Samples')
@@ -294,49 +233,3 @@ axs.set_xscale('log')
 axs.legend()
 plt.savefig("figures/MNIST_sample_runs_difference.svg")
 plt.show()
-
-    
-# Test the model
-#test_accuracy = test(model, test_loader)
-        
-#print(f'Test Accuracy: {test_accuracy}')
-"""
-# Plot the validation loss and accuracy
-fig, axs = plt.subplots(2)
-axs[0].plot(validation_losses, label='Validation Loss')
-axs[0].set_title('Validation Loss')
-axs[0].set_xlabel('Epoch')
-axs[0].set_ylabel('Loss')
-
-axs[1].plot(validation_accuracies, label='Validation Accuracy')
-axs[1].set_title('Validation Accuracy')
-axs[1].set_xlabel('Epoch')
-axs[1].set_ylabel('Accuracy')
-plt.savefig("figures/binary_MNIST.svg")
-plt.show()
-"""
-
-"""
-sample_validation_corr_losses = []
-sample_validation_corr_accuracy = []
-AL_index = indexes.clone()  # The indexes are changed in the run_experiment function
-for n in tqdm(n_samples, desc='Running sample sizes for Correlation Sampling'):
-    validation_losses, validation_accuracies = run_sequence_experiment(AL_index, n.item(), corr_sample=True)
-    sample_validation_corr_losses.append(validation_losses)
-    sample_validation_corr_accuracy.append(validation_accuracies)
-
-sample_validation_losses = []
-sample_validation_accuracy = []
-for n in tqdm(n_samples, desc='Running sample sizes for PL'):
-    validation_losses, validation_accuracies = run_sequence_experiment(indexes, n.item(), unc_sample=False)
-    sample_validation_losses.append(validation_losses)
-    sample_validation_accuracy.append(validation_accuracies)
-    
-sample_validation_unc_losses = []
-sample_validation_unc_accuracy = []
-AL_index = indexes.clone()  # The indexes are changed in the run_experiment function
-for n in tqdm(n_samples, desc='Running sample sizes for Uncertainty Sampling'):
-    validation_losses, validation_accuracies = run_sequence_experiment(AL_index, n.item(), unc_sample=True)
-    sample_validation_unc_losses.append(validation_losses)
-    sample_validation_unc_accuracy.append(validation_accuracies)
-"""
